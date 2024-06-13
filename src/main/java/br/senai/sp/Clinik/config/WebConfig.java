0e16e6dc74a78a12634de795968bdc9feba75858
unknown <gustavosilvah37@gmail.com>
2024-06-13 10:00:33 -0300

    feat: config class created and medicoController altered

---
+++ src/main/java/br/senai/sp/Clinik/config/WebConfig.java
@@ -0,0 +1,20 @@
+package br.senai.sp.Clinik.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedOrigins("http://localhost:3000")
+        .allowedHeaders("*");
+    }
+}
